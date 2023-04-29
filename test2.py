47440e1bd1b792de230a9aa4c982af7a8ef1018f
Besi <92743858+Besix2@users.noreply.github.com>
2023-04-29 13:10:35 +0200

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-from readline import set_completer
-
-
-s_condition = "Function hallo(123)"
-i_condition = s_condition.split()[1]
-i_condition = i_condition[:i_condition.find("(")] + "()"
-print(i_condition)
