08e3552f765cd0d3a3e9b57f255232512be10d13
Besi <92743858+Besix2@users.noreply.github.com>
2024-02-09 22:30:59 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,5 +4,5 @@ sudo BRANCH=next rpi-update
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
 sudo echo "libcomposite" | sudo tee -a /etc/modules
-chmod +x systemmd.sh
+chmod +x system.sh
 sudo reboot
