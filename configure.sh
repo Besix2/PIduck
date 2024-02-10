f2227d596e5d003983a615769a79f9412ae31d36
Besix2 <myusernamethis@outlook.de>
2024-02-10 15:33:33 +0000

    fixed configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 sudo BRANCH=next rpi-update
-echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt #making dwc2 available in /boot/config.txt 
 echo "dwc2" | sudo tee -a /etc/modules
-sudo echo "libcomposite" | sudo tee -a /etc/modules
-chmod +x system.sh
+sudo echo "libcomposite" | sudo tee -a /etc/modules #configuring dwc2 and libcomposite as modules
+#moving and giving permission to start-gadget.sh
+mv start_gadget.sh /usr/bin
+sudo chmod +x /usr/bin/start_gadget.sh
+mv start-gadget.service /etc/systemd/system
+#enabling hid service
+sudo systemctl enable start-gadget.service 
+sudo systemctl start start-gadget.service
 sudo reboot
--- system.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mv start_gadget.sh /usr/bin
-sudo chmod +x /usr/bin/start_gadget.sh
-new_line="/usr/bin/start_gadget.sh"
-sed -i '/fi/a '"$new_line"'' /etc/rc.local
